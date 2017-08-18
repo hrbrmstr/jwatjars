d02d01a248c72344007a30a68c269b6833b50063
Bob Rudis <bob@rud.is>
2017-08-18 14:16:18 -0400

    initial commit

---
+++ R/jwatjars-package.R
@@ -0,0 +1,13 @@
+#' Java '.jar' Files for 'jwatr'
+#'
+#' By separating out 'JAR' files from 'rJava'-based packages, such packages
+#' can have a lighter filesystem footprint, especially when they are likely to change
+#' at a more rapid pace than the underlying libraries they depend upon. Core library
+#' files from the 'Java Web Archive Toolkit' <https://sbforge.org/display/JWAT/JWAT>
+#' are included and the package version aligns with the underlying 'JAR' release version.
+#'
+#' @name jwatjars
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import rJava
+NULL
